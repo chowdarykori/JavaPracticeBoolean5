82973375ab63213ea7f8c0392aa0573159cac78a
unknown <skoripalli@gmail.com>
2019-01-16 00:41:15 +0000

    fisrt commit

---
+++ src/bootest/TryClass.java
@@ -0,0 +1,15 @@
+package bootest;
+
+
+public class TryClass {
+    public static void main(String[]args){
+
+    int x = 200;
+    int y = 100;
+    boolean result1 = (x > y); System.out.print(result1);
+    boolean result2 = (x <= y);System.out.print(result2);
+    boolean result3 = (x != y);System.out.print(result3);
+
+
+}
+    }
